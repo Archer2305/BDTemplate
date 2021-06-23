9965a64c50bcc2220d07f50ae56300e778904c45
Archer2305 <73808514+Archer2305@users.noreply.github.com>
2021-06-23 10:48:40 -0700

    Create Template.plugin.js

---
+++ Template.plugin.js
@@ -0,0 +1,23 @@
+/**
+ * @name Example
+ * @version 0.0.1
+ * @description Example Description
+ * @author Example
+ *  
+ */
+
+
+
+ module.exports = class Example{
+     
+    load() { }
+    start() {
+
+//code here
+
+//stop coding
+}
+stop(){
+
+}
+}
